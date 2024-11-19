cc8c7ad4f345729b321ede2b7cdd42e02d746100
Mario <160919611+Mariolh1121@users.noreply.github.com>
2024-11-19 11:19:21 -0600

    New setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="differential_expression_analysis",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "pydeseq2"
+    ],
+    entry_points={
+        "console_scripts": [
+            "prepare-data=analysis.scripts.prepare_data:main",
+            "run-deseq2=analysis.scripts.run_deseq2:main",
+        ],
+    },
+)
